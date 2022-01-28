837342adb787667893ab53d660b2f8ce14f1a51e
邵任升 <shaorensheng@koolearn.com>
2022-01-28 16:14:41 +0800

    占位类

---
+++ case/src/main/java/com/srs/imooc/collection/CollectionDemo.java
@@ -0,0 +1,8 @@
+package com.srs.imooc.collection;
+
+/**
+ * @author shaorensheng
+ * @date 2022/1/28
+ */
+public class CollectionDemo {
+}
---
+++ case/src/main/java/com/srs/imooc/string/StringDemo.java
@@ -0,0 +1,8 @@
+package com.srs.imooc.string;
+
+/**
+ * @author shaorensheng
+ * @date 2022/1/28
+ */
+public class StringDemo {
+}
