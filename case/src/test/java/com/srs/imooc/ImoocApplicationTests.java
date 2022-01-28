a2c26261055688574a9349e504526bb72a39ccd7
邵任升 <shaorensheng@koolearn.com>
2022-01-28 14:49:41 +0800

    imooc项目基础搭建

---
+++ case/src/test/java/com/srs/imooc/ImoocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.imooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
